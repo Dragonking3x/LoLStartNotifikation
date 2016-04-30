6bf630b5e9b0f8375ec81fff54ad44186a9d383b
Dragonking3x <Patrick__Promitzer@hotmail.com>
2016-04-30 04:27:22 +0200

    all works with 1 bug
    if I connect and disconect 2times.... it have a problem

---
+++ src/ReadMem.java
@@ -0,0 +1,24 @@
+import com.sun.jna.ptr.IntByReference;
+
+/**
+ * Created by Dragonking3x on 08.04.2016.
+ */
+public class ReadMem {
+
+
+
+
+    public ReadMem(){
+        int prozesID = 1;
+
+
+        IntByReference pid = new IntByReference(0);
+
+
+
+
+
+    }
+
+
+}
